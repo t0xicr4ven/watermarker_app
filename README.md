d139b926dbb2f7908af75ddf7392ea23caaba8e0
t0xicr4ven <57691355+t0xicr4ven@users.noreply.github.com>
2021-09-14 17:26:14 +1000

    rough version complete

--- README.md
+++ README.md
@@ -3,6 +3,15 @@ ___
 
 This application will allow you to add a watermark to selected images.
 
+Choose image from a file it will add the watermark and show you a preview.
+
+Will then ask where you want to save the new image and what to name it.
+
+Very rough version right now, could add radio buttons to ask user where to add the watermark, ie Top left, Bottom right. Also could add option to add text as the watermark or select a different image, currently can only use the image i have added.
+
+
 ## Requirments
 * Tkinter
 * pillow
+
+--
--- main.py
+++ main.py
@@ -1,23 +1,47 @@
-from tkinter import *
+from tkinter import * 
 from PIL import Image, ImageTk
 from tkinter import filedialog
 
+BACK_COLOR = '#03A9F4'
 window = Tk()
 window.title('Watermarker')
-# get user to select image
-pic = filedialog.askopenfilename()
-image_to_edit = Image.open(pic)
-o_size = image_to_edit.size
-print(f'o size is {o_size}')
-f_size = (400, 400)
-factor = min(float(f_size[1])/o_size[1], float(f_size[0])/o_size[0])
-print(f'factor is: {factor[]}')
-width = int(o_size[0]* factor)
-height = int(o_size[1]* factor)
+WATERMARK = Image.open('waterMark.png')
 
-reImg = image_to_edit.resize((width, height), Image.ANTIALIAS)
-reImg = ImageTk.PhotoImage(reImg)
-canvas = Canvas(height=500, width=500, highlightthickness=0)
-image_to_edit = canvas.create_image(250,250, image=reImg)
-canvas.grid(column=0,row=0)
+
+def quit_root():
+    window.destroy()
+
+def select_img():
+    # get user to select image
+    pic = filedialog.askopenfilename(title='Select A Image')
+    image = Image.open(pic)
+    image_size = image.size
+    #re size watermark
+    wm_re = WATERMARK.resize((round(image.size[0]*.35), round(image.size[1]*.35)))
+    wm_pos = (image_size[0] - wm_re.size[0], image_size[1] - wm_re.size[1])
+    #add watermark
+    transparent = Image.new('RGBA', image_size, (0, 0, 0, 0))
+    transparent.paste(image, (0, 0))
+    transparent.paste(wm_re, wm_pos, mask=wm_re)
+    transparent.show()
+    
+    #save image
+    save_path = filedialog.asksaveasfilename(defaultextension='.png')
+    complete_img = transparent
+    complete_img.save(save_path)
+    transparent.close()
+
+
+logo_str_1 = '{  }'
+logo_str_2 = 'Code Jar'
+canvas = Canvas(window, height=500, width=600, highlightthickness=0,
+        bg=BACK_COLOR)
+canvas.grid(columnspan=2, rowspan=1, padx=10, pady=10)
+canvas.create_text(300, 150, text=logo_str_1, font=('Arial',60))
+canvas.create_text(300, 250, text=logo_str_2, font=('Arial',60))
+#buttons
+select_img_btn = Button(window, text='Select Image', command=select_img)
+select_img_btn.grid(column=0, row=1, padx=10, pady=10)
+quit_btn = Button(window, text='Quit', command=quit_root)
+quit_btn.grid(column=1, row=1, padx=10, pady=10)
 window.mainloop()
